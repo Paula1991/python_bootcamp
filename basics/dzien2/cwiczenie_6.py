abd3b435eadbec6afe220561958f8e5f38eb7571
Paulina-1991 <paulina.pajak@o2.pl>
2018-10-29 20:31:33 +0100

    2910

--- basics/dzien2/cwiczenie_6.py
+++
@@ -1,8 +0,0 @@
-liczba = input("Podaj liczbę: ")
-
-liczba = int(liczba)
-print(f"Większa od 10: {liczba>10}")
-print(f"Mniejsza równa 15: {liczba <=15}")
-print(f"Podzielna przez 2: {liczba%2 == 0}")
-print(f"Podzielna przez 3 i większa od 10: {liczba%3 == 0 and liczba>10}")
-print(f"Większa od 10 lub podzielona przez 5: {liczba>10  or liczba%5 ==0}")
